0af21a9616a56d59c7c159efb9293c255645e645
Kyle Shive <shivekyle@gmail.com>
2018-06-27 12:01:38 -0500

    modularized code

--- ch11/analyzing_text_file_io.py
+++ ch11/analyzing_text_file_io.py
@@ -1,14 +1,18 @@
+def count_words(filename):
+	"""Count the number of words in a file, approximately"""
+
+	try:
+		with open(filename) as f_obj:
+			contents = f_obj.read()
+	except FileNotFoundError:
+		msg = "Sorry, the file " + filename + " does not exist.."
+		print(msg)
+
+	else:
+		words = contents.split()
+		num_words = len(words)
+		print("The file " + filename + " has about " + str(num_words) + " words..")
+
+
 filename = 'Dhammapada.txt'
-
-try:
-	with open(filename) as f_obj:
-		contents = f_obj.read()
-except FileNotFoundError:
-	msg = "Sorry, the file " + filename + " does not exist.."
-	print(msg)
-
-else:
-	# Count the number of words in file, approximatley
-	words = contents.split()
-	num_words = len(words)
-	print("The file " + filename + " has about " + str(num_words) + " words..")
+count_words(filename)
